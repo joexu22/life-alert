561fb2081a49b9a8192e542505b63664a744e1c6
Zein Sleiman <zein.sleiman@adtran.com>
2015-11-19 17:35:55 -0600

    Fixed double-click problem and changed colors to prettify

--- app/src/main/java/coops/lifealert/LoginPage.java
+++ app/src/main/java/coops/lifealert/LoginPage.java
@@ -1,29 +1,15 @@
 package coops.lifealert;
 
-import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
 import android.widget.Toast;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 
-/**
- * Created by zsleiman on 11/19/2015.
- */
 
 public class LoginPage extends AppCompatActivity {
     Button b1, b2;
@@ -40,28 +26,16 @@ public class LoginPage extends AppCompatActivity {
         ed2 = (EditText) findViewById(R.id.editText2);
         ed3 = (EditText) findViewById(R.id.editText3);
 
-//        b1.setOnClickListener(new View.OnClickListener() {
-//                                  @Override
-//                                  public void onClick(View v) {
-//                                      QuickstartPreferences.USERNAME = ed1.getText().toString();
-//                                      Intent intent = new Intent(LoginPage.this, MainHelpButton.class);
-//                                      startActivity(intent);
-//                                  }
-//                              }
-//        );
     }
 
     public void submit(View v) {
         b1 = (Button) findViewById(R.id.button);
-        b1.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              QuickstartPreferences.USERNAME = ed1.getText().toString();
-              QuickstartPreferences.LOCATION = ed2.getText().toString();
-              QuickstartPreferences.ISSUE = ed3.getText().toString();
-              Intent intent = new Intent(LoginPage.this, MainHelpButton.class);
-              startActivity(intent);
-          }
-      });
+        QuickstartPreferences.USERNAME = ed1.getText().toString();
+        QuickstartPreferences.LOCATION = ed2.getText().toString();
+        QuickstartPreferences.ISSUE = ed3.getText().toString();
+        Intent intent = new Intent(LoginPage.this, MainHelpButton.class);
+        startActivity(intent);
     }
+
 }
+
