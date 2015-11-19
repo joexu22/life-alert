c100bf9ccf8840926c164f4473f3170f6f15816f
BlakeHolifield <blakeaholifield@gmail.com>
2015-11-19 09:27:07 -0600

    Added help button

--- app/src/main/java/coops/lifealert/MainHelpButton.java
+++ app/src/main/java/coops/lifealert/MainHelpButton.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainHelpButton extends AppCompatActivity {
 
@@ -18,14 +19,6 @@ public class MainHelpButton extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +41,9 @@ public class MainHelpButton extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendForHelp(View view) {
+        Toast.makeText(MainHelpButton.this, "Help is on the way!", Toast.LENGTH_SHORT).show();
     }
 }
